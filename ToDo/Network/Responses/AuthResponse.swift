9f54c0dcd06c574df0fb3ddc7cd29551b7d4817f
Svetlana Polorotova <boslana25@gmail.com>
2023-11-24 09:32:49 +0300

    Complete network

---
+++ ToDo/Network/Responses/AuthResponse.swift
@@ -0,0 +1,12 @@
+//
+//  AuthResponse.swift
+//  ToDo
+//
+//  Created by Светлана Полоротова on 23.11.2023.
+//
+
+import Foundation
+
+struct AuthResponse: Decodable {
+    let accessToken: String
+}
